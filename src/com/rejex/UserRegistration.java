46292c818f956a4cb177f48fec4d292a080deff3
ShitalBhise <bhiseshital27059@gmail.com>
2022-03-11 22:38:31 +0530

    check First name starts with Cap and has minimum 3 characters

--- src/com/rejex/UserRegistration.java
+++ src/com/rejex/UserRegistration.java
@@ -1,9 +1,33 @@
 package com.rejex;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/*
+@purpose: Ability to add first name
+@param: No parameters
+@function : FirstName Validation
+@return: No return value.
+*/
+
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration System ");
+	public void FirstName() {
+		System.out.println("Enter First Name");
+		Scanner scanner = new Scanner(System.in);
+		String fN = scanner.nextLine();
 
+		/*
+		 * Ablidity to check First name starts with Cap and has minimum 3 characters
+		 * 
+		 */
+		if (Pattern.matches("[A-Z]{1}[a-z]{2,}", fN) == true)
+
+			System.out.println("Valid Input ");
+		else
+			System.out.println("Invalid Input ");
+
+		scanner.close();
 	}
+
 }
---
+++ src/com/rejex/UserRegistrationMain.java
@@ -0,0 +1,30 @@
+package com.rejex;
+
+public class UserRegistrationMain {
+	/*
+	 * @purpose: Ability to add first name
+	 * 
+	 * @param: No parameters
+	 * 
+	 * @function : FirstName Validation
+	 * 
+	 * @return: No return value.
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * Created Object
+		 * 
+		 */
+		UserRegistration R = new UserRegistration();
+
+		System.out.println("Welcome");
+
+		/*
+		 * method call
+		 * 
+		 */
+		R.FirstName();
+	}
+
+}
